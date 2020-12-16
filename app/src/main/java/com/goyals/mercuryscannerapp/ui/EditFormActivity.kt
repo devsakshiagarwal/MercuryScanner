96a356b36f011fc6da1ce6c3fd6908a67144d651
Sakshi Agarwal <sakshiagarwal@Sakshis-MacBook-Pro.local>
2020-12-16 11:05:47 +0400

    fab for inout

--- app/src/main/java/com/goyals/mercuryscannerapp/ui/EditFormActivity.kt
+++ app/src/main/java/com/goyals/mercuryscannerapp/ui/EditFormActivity.kt
@@ -2,18 +2,23 @@ package com.goyals.mercuryscannerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import com.goyals.mercuryscannerapp.R
 import com.goyals.mercuryscannerapp.model.schema.AadharInfo
 import kotlinx.android.synthetic.main.activity_edit_form.*
-import kotlinx.android.synthetic.main.app_bar_main.toolbar
 
 class EditFormActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_edit_form)
     initViews()
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_save, menu)
+    return true
   }
 
   override fun onMenuItemClick(item: MenuItem?): Boolean {
@@ -36,14 +41,15 @@ class EditFormActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
 
   private fun initViews() {
     actionBar?.setDisplayHomeAsUpEnabled(true)
-    //toolbar.inflateMenu(R.menu.menu_save)
-    val aadharInfo = intent.getParcelableExtra<AadharInfo>("aadhar_info")!!
-    et_uid.setText(aadharInfo.uid)
-    et_name.setText(aadharInfo.name)
-    et_dob.setText(aadharInfo.dob)
-    et_yob.setText(aadharInfo.yob)
-    et_address.setText(aadharInfo.address)
-    et_age.setText(aadharInfo.age)
-    et_gender.setText(aadharInfo.gender)
+    if (intent.getParcelableExtra<AadharInfo>("aadhar_info") != null) {
+      val aadharInfo = intent.getParcelableExtra<AadharInfo>("aadhar_info")!!
+      et_uid.setText(aadharInfo.uid)
+      et_name.setText(aadharInfo.name)
+      et_dob.setText(aadharInfo.dob)
+      et_yob.setText(aadharInfo.yob)
+      et_address.setText(aadharInfo.address)
+      et_age.setText(aadharInfo.age)
+      et_gender.setText(aadharInfo.gender)
+    }
   }
 }
